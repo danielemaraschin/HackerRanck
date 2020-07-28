029f272c999653d9c2d9ec03497cc787e6fd9bac
danielemaraschin <danikmaraschin@gmail.com>
2020-07-28 22:35:43 +0200

    Functions and return

---
+++ birthdayCandles.js
@@ -0,0 +1,21 @@
+
+// imprimir quantidade de números
+// Complete the birthdayCakeCandles function below.
+
+let totaldeVelas = 4;
+
+
+var ar = new Array(4);
+ar[0] = 3;
+ar[1] = 2;
+ar[2] = 1;
+ar[3] = 3;
+
+
+
+
+
+for (var i = 0; i < totaldeVelas; i++) {
+    console.log(ar[i])
+}
+
