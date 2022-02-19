5ae4fea7fce31a38596e8043ccfd2d5651c58a3b
danielemaraschin <danikmaraschin@gmail.com>
2022-02-19 16:39:47 +0100

    sum according to index of array

--- sumIndex.js
+++ sumIndex.js
@@ -1,1 +1,12 @@
-//somar somente 
+//somar somente os index ímpares
+var arr = [1,7,6,4,5]
+var somaArrSemPar = 0
+var soma = 0
+
+
+for(var i = 0; i< arr.length; i++){
+    if(i%2 != 0){
+        soma += arr[i];
+    }
+}
+console.log(soma)
