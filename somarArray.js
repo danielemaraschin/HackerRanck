3ee9e286471f01a5975abd161002510331b3b849
danielemaraschin <danikmaraschin@gmail.com>
2022-02-19 16:45:00 +0100

    sum according to index of array

---
+++ somarArray.js
@@ -0,0 +1,40 @@
+var arr= [6, 90, 2, 3, 4];
+var somaMaior = 0;
+var somaMenor = 0;
+
+
+var soma = 0;
+for(var i = 0; i < arr.length; i++) {
+    soma += arr[i];
+}
+console.log(soma);
+
+// var resultadoArr = 0;
+
+//     for(index = 0; index < arr.length;
+//          index++){
+//       var arrayMenosUm =  arr.splice(0,1)
+         
+//         console.log(resultadoArr)   
+//     }
+
+
+
+    // console.log(resultadoArr)
+
+// for(var i = 0; i < arr.length; i++){
+//         var valorNoArray = arr[i];
+//        if (valorNoArray < 0 ){
+//            negativo = negativo + 1;
+//        }else if(valorNoArray > 0){
+//            positivo = positivo +1;
+//        }else{
+//            nulo = nulo +1;
+       
+//         }
+//    }
+//    var primeiroElemento = positivo/arr.length ;
+//    var segundoElemento = negativo/arr.length;
+//    var terceiroElemento = nulo/arr.length;
+
+//isso nao esta pronto e eh uma mistura de exercicios
